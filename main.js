cdf4915da5a9f624c41a5b0ba4855705275d6e7a
JebusDev <antoniozuniga.dev@gmail.com>
2023-02-02 23:50:32 -0700

    limpiando el tablero

---
+++ main.js
@@ -0,0 +1,20 @@
+
+const newGameButton = document.querySelector('#newGame');
+const tablero = document.querySelectorAll('#spot');
+
+const printChart = () => {
+    
+}
+
+const gameInit = () => {
+    for (const element of tablero) {
+        element.firstElementChild.remove();
+    }
+}
+
+const toggleTurn = () => {
+
+}
+
+gameInit();
+
