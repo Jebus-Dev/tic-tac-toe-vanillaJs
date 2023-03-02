add400ddf0fbbe42576789daf52735a0b5c710a1
JebusDev <antoniozuniga.dev@gmail.com>
2023-03-01 23:59:10 -0700

    moviendo logica del boton reiniciar juego

--- events.js
+++ events.js
@@ -1,40 +1,26 @@
 import {playerOne, playerTwo} from './modules/players.js'
-import {toggleVisiblePlayerTurn,  logicSpot, turn, toggleTurn, hasWon, restartLogicSpaces, turnTrue, winner} from './modules/logic.js'
+import {toggleVisiblePlayerTurn,  logicSpot, turn, toggleTurn, hasWon,  printWinner, restart, finishedGame} from './modules/logic.js'
 
 const modal = document.querySelector('#modal')
-const btnAbrirModal = document.querySelector('#btnAbrirModal')
 const btnCerrarModal = document.querySelector('#btnCerrarModal')
-const slots = document.querySelectorAll('.spot');
 
 export const restartGame = () => {
-    restartLogicSpaces()
-    for (const element of slots) {
-        if (element.firstElementChild) {
-            console.log('hola')
-            element.firstElementChild.remove();
-        }
-    }
-    turnTrue();
+    restart();
 }
 
 
 export const print = (e) => {
-
     let spaceClass = e.target.className;
     let space  = e.target.id;
 
     if ( spaceClass == 'spot' ) { 
-        turn == true ? (playerOne(space))
+        turn == true ? playerOne(space)
                      : playerTwo(space);
         logicSpot(space)
-
-        hasWon() ? (modal.showModal, console.log(winner())) : (toggleTurn(), toggleVisiblePlayerTurn());
-
+        hasWon() ? (modal.showModal(), printWinner(), finishedGame()) : (toggleTurn(), toggleVisiblePlayerTurn());
+        
     }
-
 }
-
-
 
 btnCerrarModal.addEventListener('click', (e) => {
     modal.close();
