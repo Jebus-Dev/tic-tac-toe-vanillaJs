2cbcbd3c5f7777b06af738d0c5aafb5f9689ef3f
JebusDev <antoniozuniga.dev@gmail.com>
2023-03-07 23:55:47 -0700

    limpiando el archivo de eventos

--- events.js
+++ events.js
@@ -2,9 +2,7 @@ import { restart, print} from './modules/logic.js'
 
 const board = document.querySelector("#chartContainer");
 const btnRestartGame = document.querySelector('#restart');
-const btnNextRound = document.querySelector('#next-round');
-const modal = document.querySelector('#modal');
-const btnCerrarModal = document.querySelector('#btnCerrarModal');
+
 
 export const game = () => {
 
@@ -16,13 +14,5 @@ export const game = () => {
         print(e);
     })
 
-    btnCerrarModal.addEventListener('click', () => {
-        modal.close(); 
-    })
-
-    btnNextRound.addEventListener('click', () => {
-        restart();
-        modal.close();
-    })
-
+    
 }
