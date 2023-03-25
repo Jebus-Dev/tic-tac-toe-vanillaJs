c04bb11d8398c88821361abb5b1c6f6d9de36a92
JebusDev <antoniozuniga.dev@gmail.com>
2023-03-25 02:01:53 -0700

    Logica cambiar modooscuro/claro | archivo temporal

--- events.js
+++ events.js
@@ -1,18 +1,40 @@
-import { restart, print} from './modules/logic.js'
+import { restart, print, logicSpaces, boardStatus} from './modules/logic.js'
 
-const board = document.querySelector("#chartContainer");
+const board = document.querySelector('#chartContainer');
 const btnRestartGame = document.querySelector('#restart');
+const options = document.querySelectorAll('input[name="mode"]');
+let mode;
 
 
 export const game = () => {
-
     btnRestartGame.addEventListener('click', () => {
         restart();
+        options.forEach( button => {
+            button.disabled = false;
+        });
+        
     })
-
-    board.addEventListener('click', (e) => {
-        print(e);
-    })
-
     
+    board.addEventListener('click', (e) => {
+        mode = document.querySelector('input[name="mode"]:checked').value;
+        
+        print(e, mode);
+
+        if (logicSpaces.some(boardStatus)) {
+            options.forEach( buttons => {
+                buttons.disabled = true;
+            });
+        }
+    })
+
+    const toggleColor = document.querySelector('#color-toggle');
+
+    toggleColor.addEventListener('click', () => {
+        toggleColor.checked ? setUserTheme('dark') : setUserTheme('light');  
+    })
+
+    const setUserTheme = (newTheme) => {
+        document.documentElement.setAttribute('data-theme', newTheme);
+    }
+
 }
